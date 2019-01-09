831f22d112b13cbf4085a17dbc85080fa31c4153
Arun679 <thakurarun679@gmail.com>
2019-01-09 17:34:26 +0530

    Update MainActivity.java

--- app/src/main/java/awedroid/com/demoapp/MainActivity.java
+++ app/src/main/java/awedroid/com/demoapp/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        //Hello
+        //Hello From Server
     }
 
     @Override
